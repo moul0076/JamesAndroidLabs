b2bfed12f36cede5b8548e5eef0fa7e49fddd293
James Mouland <selendrasama@hotmail.com>
2021-09-23 11:54:08 -0400

    Week 2 lab part 3 of 4

--- app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
@@ -9,21 +9,27 @@
 package algonquin.cst2335.moul0076;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView myText = findViewById(R.id.textview);
-
         myText.setText("Java put this here");
 
         Button btn = findViewById(R.id.mybutton);
@@ -39,6 +45,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        CheckBox CB = findViewById(R.id.boxChecked);
+        Switch Sw = findViewById(R.id.toggled);
+        RadioButton RB =findViewById(R.id.radioButton);
+
+        CB.setOnCheckedChangeListener(( b, isChecked) ->{
+
+            RB.setChecked(isChecked);
+            Sw.setChecked(isChecked);
+
+            Toast.makeText(MainActivity.this,"You clicked on Check Box and is now: " + isChecked, Toast.LENGTH_LONG).show();
+
+        });
+        Sw.setOnCheckedChangeListener(( b, isChecked) ->{
+
+            RB.setChecked(isChecked);
+            CB.setChecked(isChecked);
+
+            Toast.makeText(MainActivity.this,"You clicked on Switch and is now: " + isChecked, Toast.LENGTH_LONG).show();
+
+        });
+        RB.setOnCheckedChangeListener(( b, isChecked) ->{
+
+            Toast.makeText(MainActivity.this,"You clicked on Radio Button and is now: " + isChecked, Toast.LENGTH_LONG).show();
+
+        });
 
 
     }
