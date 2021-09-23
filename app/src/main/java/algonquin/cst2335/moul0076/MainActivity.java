be5532bbbe6a2820253725a1772e03cbe89fba72
James Mouland <selendrasama@hotmail.com>
2021-09-22 23:43:58 -0400

    Week 2 lab part 2 of 4

--- app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
@@ -1,8 +1,19 @@
+/* James Android Lab
+ * Author: James Mouland
+ * ID: 040418260
+ * For: Eric Torunski
+ * This is the lab for 21F_CST2335_010 Mobile Graphical Interface Programing
+ * This git is the Week 2 branch on GIT hub
+ */
+
 package algonquin.cst2335.moul0076;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myText = findViewById(R.id.textview);
+
+        myText.setText("Java put this here");
+
+        Button btn = findViewById(R.id.mybutton);
+        //btn.setText("the view was previous" + oldText);
+
+        EditText myedit = findViewById(R.id.myEditText);
+        String editString = myedit.getText().toString();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myText.setText("edit text has " + myedit.getText() );
+            }
+        });
+
+
+
     }
+
 }
