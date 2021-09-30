d0d2cc4254675b6eb1390cca6a64f2fd60770b4b
moul0076 <90407336+moul0076@users.noreply.github.com>
2021-09-30 12:58:04 -0400

    Week 3: working on step 3 Error can not find symbol: Animation.RELATIVE_TO_SELF

--- app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
@@ -2,13 +2,44 @@ package algonquin.cst2335.moul0076;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.ImageView;
+import android.view.animation.RotateAnimation;
+import android.view.animation.LinearInterpolator;
+//import android.widget.ViewAnimator;
 
+import android.os.Bundle;
+//RELATIVE_TO_SELF
+//symbol:   variable Animation
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    Switch sw;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.flagveiw);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) ->
+        {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else
+            {
+                imgView.clearAnimation();
+            }
+        });
     }
 }
