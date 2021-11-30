aaf614a2a34af454015d6bf5788ba4f162d5a5cd
moul0076 <90407336+moul0076@users.noreply.github.com>
2021-11-29 23:27:00 -0500

    Week 9 - Part 2 almost working Icon issues

--- app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/moul0076/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import android.util.*;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -59,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         Button forecastBtn = findViewById(R.id.forecastButton);
         EditText cityText = findViewById(R.id.cityTextField);
 
+        //JSONObject theDocument;
+
         forecastBtn.setOnClickListener( (click) ->
         {
             Executor newThread = Executors.newSingleThreadExecutor();
@@ -84,6 +88,82 @@ public class MainActivity extends AppCompatActivity {
                         new InputStreamReader(in, StandardCharsets.UTF_8)))
                         .lines()
                         .collect(Collectors.joining("\n"));
+                //JSONArray theArray = new JSONArray( text );
+                try {
+                    JSONObject theDocument = new JSONObject(text);
+                    JSONArray weatherArray = theDocument.getJSONArray ("weather");
+                    JSONObject position0 = weatherArray.getJSONObject(0);
+
+                    String description = position0.getString("description");
+                    String iconName = position0.getString("icon");
+
+                    JSONObject mainObject = theDocument.getJSONObject("main");
+
+                    double current = mainObject.getDouble("temp");
+                    double min = mainObject.getDouble("temp_min");
+                    double max = mainObject.getDouble("temp_max");
+                    double humitidy = mainObject.getInt("humidity");
+
+
+
+                    Bitmap image = null;
+                    URL imgUrl = new URL("https://openweathermap.org/img/w/"+ iconName +".png");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == 200)
+                    {
+                        image = BitmapFactory.decodeStream(connection.getInputStream());
+
+                        ImageView iv = findViewById(R.id.icon);
+                        iv.setImageBitmap(image);
+                    }
+
+                    FileOutputStream fOUt = null;
+                    try {
+                        fOUt = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
+                        image.compress(Bitmap.CompressFormat.PNG, 100, fOUt);
+                        fOUt.flush();
+                        fOUt.close();
+                    } catch ( FileNotFoundException e){
+                        e.printStackTrace();
+                    }catch ( NullPointerException e){
+                        e.printStackTrace();
+                    }
+
+                    runOnUiThread( (  )  -> {
+                        TextView tv = findViewById(R.id.temp);
+                        tv.setText("The current temperature is " + current);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.minTemp);
+                        tv.setText("The minimum temperature is " + min);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.maxTemp);
+                        tv.setText("The maximum temperature is " + max);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.humitidy);
+                        tv.setText("The minimum temperature is " + humitidy);
+                        tv.setVisibility(View.VISIBLE);
+
+                    //    ImageView iv = findViewById(R.id.icon);
+                    //    iv.setImageBitmap(image);
+                    //    iv.setVisibility(View.VISIBLE);
+                    });
+
+
+
+
+                }catch (JSONException je)
+                {
+                    Log.e("JSON error", je.getMessage());
+                }
+
+
+
+
 
 
             }
